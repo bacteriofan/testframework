3b204eaf991fdb308bd2162a065ffacd939cdf30
Kseniia_Stepanova <kseniia_stepanova@epam.com>
2020-05-01 15:07:25 +0300

    added sandbox options

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,18 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test {
 
     @org.testng.annotations.Test
     public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.navigate().to("https://hamwaves.com/collaboration/doc/rypress.com/index.html");
         System.out.println("Browser title is " + driver.getTitle());
         driver.quit();
