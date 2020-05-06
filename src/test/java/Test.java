1fe16953b6d3099a8080664db489880d6b658249
Kseniia_Stepanova <kseniia_stepanova@epam.com>
2020-05-06 08:15:49 +0300

    updated website

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,10 +14,20 @@ public class Test {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
-        driver.navigate().to("https://hamwaves.com/collaboration/doc/rypress.com/index.html");
+
+
+        driver.navigate().to("https://developer.mozilla.org/en-US/docs/Web/Tutorials");
+
         System.out.println("Browser title is: " + driver.getTitle());
+
+        driver.findElement(By.xpath("//a[text()[contains(.,'Introduction to HTML')]]")).click();
+        String header = driver.findElement(By.cssSelector(".title")).getText();
+
+        System.out.println("Opened page:" + header);
+
         driver.quit();
     }
 }
